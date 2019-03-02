cfed46b97554a339aa0f08ce219b7b084268d3e5
Ranjeet-Raushan <37865490+Ranjeet-Raushan@users.noreply.github.com>
2019-03-03 02:50:42 +0530

    Update AppDelegate.swift

--- ClickableUILable-UItapGesture/AppDelegate.swift
+++ ClickableUILable-UItapGesture/AppDelegate.swift
@@ -15,9 +15,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 }
-
-/*
- Future Reference :
- https://www.dropbox.com/s/083qkqq01f9ds2e/ClickableUILable-UItapGesture.zip?dl=0
- https://www.youtube.com/watch?v=aaW5rxd5rbw  -  [ Make UIlable clickable in swift using UITapGestureRecognizer ]
- */
