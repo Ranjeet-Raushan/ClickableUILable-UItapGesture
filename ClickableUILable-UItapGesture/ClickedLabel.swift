c560be39d2537213e84b79340c644c41b2eff6db
Ranjeet Raushan <rkraushansdm@gmail.com>
2019-03-03 02:44:31 +0530

    made few changes in UI and ViewController class

---
+++ ClickableUILable-UItapGesture/ClickedLabel.swift
@@ -0,0 +1,13 @@
+//  ClickedLabel.swift
+//  ClickableUILable-UItapGesture
+//  Created by Ranjeet Raushan on 03/03/19.
+//  Copyright © 2019 Aman Aggarwal. All rights reserved.
+
+import UIKit
+
+class ClickedLabel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ClickableUILable-UItapGesture/ViewController.swift
+++ ClickableUILable-UItapGesture/ViewController.swift
@@ -1,25 +1,38 @@
-//
 //  ViewController.swift
 //  ClickableUILable-UItapGesture
-//
-//  Created by Aman Aggarwal on 22/01/18.
-//  Copyright © 2018 Aman Aggarwal. All rights reserved.
-//
+//  Created by Ranjeet Raushan on 28/02/2019.
+//  Copyright © 2019 Vaayoo. All rights reserved.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelClickableUsingTapGesture: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelClickableUsingTapGesture.text = "labelClickableUsingTapGesture"
+        self.view.addSubview(labelClickableUsingTapGesture)
+        
+        //to make UILabel clickable
+        labelClickableUsingTapGesture.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(clickLable))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        labelClickableUsingTapGesture.addGestureRecognizer(tapGesture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK:- clickLable
+    @objc func clickLable() {
+        let clIckedLaBel = storyboard?.instantiateViewController(withIdentifier: "clickedlabel") as! ClickedLabel
+        navigationController?.pushViewController(clIckedLaBel, animated: true)
+        
     }
-
-
+    
 }
 
+/*
+Future Reference :
+https://www.dropbox.com/s/083qkqq01f9ds2e/ClickableUILable-UItapGesture.zip?dl=0
+https://www.youtube.com/watch?v=aaW5rxd5rbw  -  [ Make UIlable clickable in swift using UITapGestureRecognizer ]
+*/
